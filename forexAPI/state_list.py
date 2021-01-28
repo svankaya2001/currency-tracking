f37f6d2d814ea77a742da559506c2f7b4820a305
Shreyas Vankayalapati <59900350+svankaya2001@users.noreply.github.com>
2021-01-28 13:08:07 -0800

    Delete state_list.py

--- forexAPI/state_list.py
+++
@@ -1,6 +0,0 @@
-states = ['new-york', 'california', 'texas', 'florida', 'illinois', 'massachusetts', 'pennsylvania', 'georgia', 'arizona', 'michigan', 'maryland', 'north-carolina', 'virginia', 
-'louisiana', 'ohio', 'connecticut', 'indiana', 'tennessee', 'alabama', 'south-carolina', 'minnesota', 'washington', 'colorado', 'iowa',
-'wisconsin', 'mississippi', 'utah', 'arkansas', 'missouri', 'nebraska', 'nevada', 'rhode-island', 'kentucky', 'kansas', 'oklahoma',
-'new-mexico', 'delaware', 'district-of-columbia', 'oregon', 'puerto-rico', 'south-dakota', 'idaho', 'new-hampshire', 'north-dakota', 
-'maine', 'west-virginia', 'wyoming', 'vermont', 'montana', 'alaska', 'hawaii' 
-]
